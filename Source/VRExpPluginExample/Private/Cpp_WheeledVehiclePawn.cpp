75d2fdadbfcc29ad3a754c92ceffed3a2d76649d
lch9527 <38733164+lch9527@users.noreply.github.com>
2023-06-29 13:21:41 -0700

    fix bug

--- Source/VRExpPluginExample/Private/Cpp_WheeledVehiclePawn.cpp
+++ Source/VRExpPluginExample/Private/Cpp_WheeledVehiclePawn.cpp
@@ -4,6 +4,9 @@
 #include "Cpp_WheeledVehiclePawn.h"
 #include "VRExpansionPlugin//Public/Interactibles/VRLeverComponent.h"
 #include "VRExpansionPlugin/Public/VRBaseCharacter.h"
+#include "Components/ArrowComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
 #include "Kismet/GameplayStatics.h"
 
 ACpp_WheeledVehiclePawn::ACpp_WheeledVehiclePawn()
@@ -16,24 +19,94 @@ ACpp_WheeledVehiclePawn::ACpp_WheeledVehiclePawn()
 
 	Driver_pos = CreateDefaultSubobject<USceneComponent>(TEXT("Driver_set"));
 
-	R_lever_base = CreateDefaultSubobject<USceneComponent>(TEXT("R_lever_base"));
+	R_lever_base = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("R_lever_base"));
 	
-	L_lever_base = CreateDefaultSubobject<USceneComponent>(TEXT("L_lever_base"));
+	L_lever_base = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("L_lever_base"));
 
 	L_lever = CreateDefaultSubobject<UVRLeverComponent>(TEXT("L_lever"));
 
 	R_lever = CreateDefaultSubobject<UVRLeverComponent>(TEXT("R_lever"));
 
-	R_lever_base->SetupAttachment(Upper);
-	L_lever_base->SetupAttachment(Upper);
+	tip3 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Moveable_Tip3"));
+
+	tip2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Moveable_Tip2"));
+
+	tip1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Moveable_Tip1"));
+
+	tip = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Moveable_Tip"));
+
+	tip_move = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("tip_move"));
+
+	Right_tip_low = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right_tip_low"));
+
+	Right_tip_up = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right_tip_up"));
+
+	Left_tip = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left_tip"));
+
+	Left_tip_up = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left_tip_up"));
+
+	Left_tip_low = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left_tip_low"));
+
+	RIght_tip = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RIght_tip"));
+
+	Tip_move_physice = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("Tip_move_physice"));
+
+	Right_arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("R_arrow"));
+
+	Left_arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("L_arrow"));
+
+	R_lever_tip = CreateDefaultSubobject<USceneComponent>(TEXT("RLever_tip"));
+
+	L_lever_tip = CreateDefaultSubobject<USceneComponent>(TEXT("LLever_tip"));
+
+	RootComponent = GetMesh();
+
+	Base->SetupAttachment(GetMesh());
 
 	Upper->SetupAttachment(Base);
 
-	Driver_pos->SetupAttachment(Upper);
-	
-	L_lever->SetupAttachment(L_lever_base);
+	R_lever_base->SetupAttachment(Upper);
+	L_lever_base->SetupAttachment(Upper);
 
+	tip3->SetupAttachment(Upper);
+	tip2->SetupAttachment(tip3);
+	tip1->SetupAttachment(tip2);
+	tip->SetupAttachment(tip1);
+
+	tip_move->SetupAttachment(tip);
+	RIght_tip->SetupAttachment(tip_move);
+	Right_tip_low->SetupAttachment(tip_move);
+	Right_tip_up->SetupAttachment(tip_move);
+	Left_tip->SetupAttachment(tip_move);
+	Left_tip_low->SetupAttachment(tip_move);
+	Left_tip_up->SetupAttachment(tip_move);
+
+	Tip_move_physice->SetupAttachment(tip1);
+
+	Driver_pos->SetupAttachment(Upper);
+	L_lever->SetupAttachment(L_lever_base);
 	R_lever->SetupAttachment(R_lever_base);
+
+	
+	Right_arrow->SetupAttachment(R_lever_base);
+	Left_arrow->SetupAttachment(L_lever_base);
+
+	R_lever_tip->SetupAttachment(R_lever);
+
+	L_lever_tip->SetupAttachment(L_lever);
+
+
+	if (ULocalPlayer* LocalPlayer = Cast<ULocalPlayer>(UGameplayStatics::GetPlayerController(GetWorld(), 0)))
+	{
+		/*if (UEnhancedInputLocalPlayerSubsystem* InputSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (!InputMapping.IsNull())
+			{
+				InputSystem->AddMappingContext(InputMapping.LoadSynchronous(), Priority);
+			}
+		}*/
+	}
+
 
 }
 
@@ -50,15 +123,34 @@ FTransform ACpp_WheeledVehiclePawn::R_Control()
 
 void ACpp_WheeledVehiclePawn::BeginPlay()
 {
-	
-	ACharacter* get_player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
-	
-	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("LLLLLLLLLLLLLLLLLL %f"), GetWorld()->TimeSeconds));
-	if (AVRBaseCharacter* player = Cast<AVRBaseCharacter>(get_player))
+	if (ULocalPlayer* LocalPlayer = Cast<ULocalPlayer>(UGameplayStatics::GetPlayerController(GetWorld(), 0)))
 	{
-		player->SetSeatedMode(Driver_pos, true, player->VRProxyComponent->GetRelativeTransform(), FTransform(), 40.0, 20.0, true);
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("World delta for current frame equals %f"), GetWorld()->TimeSeconds));
+		/*if (UEnhancedInputLocalPlayerSubsystem* InputSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (!InputMapping.IsNull())
+			{
+				InputSystem->AddMappingContext(InputMapping.LoadSynchronous(), Priority);
+			}
+		}*/
 	}
+
+
+	AVRBaseCharacter* player;
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, [&]()
+		{
+			player = Cast<AVRBaseCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+			if (player != nullptr)
+			{
+				player->SetSeatedMode(Driver_pos, true, player->VRProxyComponent->GetRelativeTransform(), FTransform(), 40.0, 20.0, true);
+				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("in set")));
+			}
+			
+		}, 3, false);
+
+
+
+	
 	
 	
 
--- Source/VRExpPluginExample/Public/Cpp_WheeledVehiclePawn.h
+++ Source/VRExpPluginExample/Public/Cpp_WheeledVehiclePawn.h
@@ -4,9 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "WheeledVehiclePawn.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
 #include "Cpp_WheeledVehiclePawn.generated.h"
 
 class UVRLeverComponent;
+class UArrowComponent;
+class UInputMappingContext;
 
 /**
  * 
@@ -30,16 +34,72 @@ public:
 		USceneComponent* Driver_pos;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = lever_vir)
-		USceneComponent* R_lever_base;
+		USceneComponent* R_lever_tip;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = lever_vir)
-		USceneComponent* L_lever_base;
+		USceneComponent* L_lever_tip;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = lever_vir)
+		UStaticMeshComponent* R_lever_base;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = lever_vir)
+		UStaticMeshComponent* L_lever_base;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = lever_vir)
 		UVRLeverComponent* L_lever;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = lever_vir)
 		UVRLeverComponent* R_lever;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = lever_vir)
+		UArrowComponent* Right_arrow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = lever_vir)
+		UArrowComponent* Left_arrow;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* tip3;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* tip2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* tip1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* tip;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* tip_move;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* Right_tip_low;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* Right_tip_up;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* Left_tip;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* Left_tip_up;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* Left_tip_low;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UStaticMeshComponent* RIght_tip;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tip)
+		UPhysicsConstraintComponent* Tip_move_physice;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+		UInputMappingContext* InputMapping;
+
+
+	
 	
 	float Move_speed = 0;
 
